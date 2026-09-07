65ce8cfae22ea121e7104748c016408cad084683
e-shekharreddy <shekharreddy.eanala.com>
2026-09-07 14:07:10 +0530

    python basics

--- sets.py
+++ sets.py
@@ -1,10 +1,10 @@
 sample = set()
-print(sample,type(sample))
+# print(sample,type(sample))
 
 #A set consists of unique values only and get unordred collections -> there is no order gurantee in
 # beacause of this a set does not supports index
 sample = {'a', 'b', 'b', 'c', 'c'}
-print(sample)
+# print(sample)
 # print(dir(sample))
 
 """
@@ -12,4 +12,11 @@ print(sample)
 """
 # print(sample[0]) # throw the error 'set' object is not subscriptable
 
+sample.add(1) # This prove that set is a mutable dststype
+# print(sample)
 
+set1 = {'a', 'b', 'c'}
+set2 = {1, 'a', 'b', 'c'}
+# print(set1, set2)
+# print(set1.intersection(set2))
+print(set2.difference(set1))
